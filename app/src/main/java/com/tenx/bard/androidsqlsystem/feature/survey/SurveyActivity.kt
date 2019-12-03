28780aeb412a3a1837637c5bfcc640c0830f0d76
Cakewalker1337 <max.kovalev15@gmail.com>
2019-12-03 19:00:37 +0700

    Add survey activity

---
+++ app/src/main/java/com/tenx/bard/androidsqlsystem/feature/survey/SurveyActivity.kt
@@ -0,0 +1,13 @@
+package com.tenx.bard.androidsqlsystem.feature.survey
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.tenx.bard.androidsqlsystem.R
+
+class SurveyActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_survey)
+    }
+}
